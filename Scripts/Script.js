c485d793eae7319268341d09ebe99cd1b381a0e4
Jonata Silva <jonatasilva118@gmail.com>
2023-01-06 21:38:36 -0300

    Implementação, voltar ao topo Btn

---
+++ Scripts/Script.js
@@ -0,0 +1,17 @@
+// Quando o usuário rolar 20 pixels para baixo a partir do topo da página, mostre o botão
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// Quando o usuário clicar no botão, role de volta ao topo da página
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
