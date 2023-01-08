40b24a947fe8cd387e30ef05a97015211ba2f92a
Jonata Silva <jonatasilva118@gmail.com>
2023-01-08 10:49:26 -0300

    Update  comments at Scripts.js

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,4 +1,4 @@
-// Quando o usuário rolar 20 pixels para baixo a partir do topo da página, mostre o botão
+// Quando o usuário rolar 20 pixels para baixo a partir do topo da página, mostre o botão.
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -7,20 +7,17 @@ function scrollFunction() {
   } else {
     document.getElementById("myBtn").style.display = "none";
   }
-}
-
-// Quando o usuário clicar no botão, role de volta ao topo da página
+} // Quando o usuário clicar no botão, role de volta ao topo da página.
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-}
-// Fim do código do botão VOLTAR AO TOPO
+} // Fim do código do botão VOLTAR AO TOPO.
+
+// Quando clicar na imagem de um projeto, abrirá uma pop-up.
 function openPopup() {
   let popup = document.querySelector('.popUp');
   popup.classList.add('popUp-Open');
-}
-
-function closePopup() {
+} function closePopup() { // Quando clicar no "x" a pop-up será fechada.
   let popup = document.querySelector('.popUp');
   popup.classList.remove('popUp-Open');
 }
